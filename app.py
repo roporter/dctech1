0ce0ed936ad76b2ce39e8d53fed7a2b9dead93cc
roporter <roporter@cisco.com>
2016-04-08 06:30:12 +0000

    Web page reset to Hello World

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<!DOCTYPE html><html lang='en'><head><link href='css/bootstrap.min.css' rel='stylesheet'><script src='js/bootstrap.min.js'></script></head><body><div class='container'><div class='row'><div class='col-xs-2 col-md-2'></div><div class='col-xs-6 col-md-4'><img src='img/TechHuddleLogo.png'></div><div class='col-xs-6 col-md-4'></div></div><div class='row' style='height:60px'><div class='col-md-2'>&nbsp</div></div><div class='row' style='align:center'><div class='col-xs-3 col-md-3'></div><div class='col-xs-4 col-md-4'><a href='http://drone04.shipped-cisco.com/roporter/dctech1'><img src='http://drone04.shipped-cisco.com/api/badges/roporter/dctech1/status.svg' /></a></div></div><div class='row' style='height:60px'><div class='col-md-2'>&nbsp</div></div><div class='row' style='align:center'><div class='col-xs-3 col-md-3'></div><div class='col-xs-12 col-md-8' style='font-size:32px'>Welcome to the Tech Huddle April 2016!!!!</div></div></div></body></html>"
-
+    return "Hello World!!"
 
 
 if __name__ == "__main__":
